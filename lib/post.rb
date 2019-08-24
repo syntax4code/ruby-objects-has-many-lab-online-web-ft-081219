f7fa40331ad8d143948728defd44c7a2d12f97cf
Warren Harper II <warrenharper2@att.net>
2019-08-24 23:50:31 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,18 @@
 class Post
-
   attr_accessor :title, :author
+
+  @@all = []
+
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+    author.name if author
   end
 end
